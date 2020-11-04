4d383021ad5f5b58a916523792f60cd593be5a7e
jojahn <jjahns.mail@gmail.com>
2020-11-04 17:49:43 +0100

    started implementation of lambda calculus

--- src/elixir/lib/lambda_calculus/lambda_calculus.ex
+++ src/elixir/lib/lambda_calculus/lambda_calculus.ex
@@ -1,20 +1,32 @@
-defmodule KPEX.LambdaCalculus do
-  @moduledoc false
+IO.puts("Lambda Calculus")
 
-  def hello_world
-
-  @spec false_fn(((any) -> any), ((any) -> any)) :: ((any) -> any)
-  def true_fn(x, y) do
-    return y
+defmodule LambdaCalculus do
+  def t(x) do
+    fn(_y) -> x end
   end
-
-  @spec true_fn(((any) -> any), ((any) -> any)) :: ((any) -> any)
-  def true_fn(x, y) do
-    return x
-  end
-
-  defp private() do
-    return 0
-  end
-
 end
+
+t = LambdaCalculus.t
+f = fn(_x, y) -> y end
+i = fn x -> x end
+m = fn x -> x.(x) end
+
+# Helper
+functions = %{
+  "#{Kernel.inspect t}" => "True",
+  "#{Kernel.inspect f}" => "False",
+  "#{Kernel.inspect i}" => "ID",
+  "#{Kernel.inspect m}}" => "M"
+}
+get_name = fn x -> functions["#{Kernel.inspect x}"] end
+
+# Examples
+IO.puts "ID = #{get_name.(i)}"
+IO.puts "True = #{get_name.(t)}"
+IO.puts "False = #{get_name.(t)}"
+IO.puts "ID(True) = #{get_name.(i.(t))}"
+IO.puts "ID(False) = #{get_name.(i.(f))}"
+IO.puts "ID(ID) = #{get_name.(i.(i))}"
+IO.puts "M(ID) = #{get_name.(m.(i))}"
+IO.puts "M(True) = #{get_name.(m.(t))}"
+# IO.puts "M(M) = #{get_name.(m.(m))}"
--- src/elixir/lib/lambda_calculus/simple.ex
+++
@@ -1,8 +0,0 @@
-IO.puts("")
-
-true_fn = fn(x, y) -> return x end
-false_fn = fn(x, y) -> return y end
-
-id_fn = fn(x) -> return x end
-
-id_fn(true_fn)
